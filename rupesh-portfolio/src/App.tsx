2270a488861f7ae45587c74715a15d0ee3d23ab0
pmrupeshshinde <136807056+pmrupeshshinde@users.noreply.github.com>
2025-09-16 13:07:39 +0530

    Add files via upload

---
+++ rupesh-portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portfolio from './Portfolio';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
